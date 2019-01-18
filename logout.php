7b4d47a129394e5ea75029105fe3dce42039b17f
Liverbreaker <46372161+Liverbreaker@users.noreply.github.com>
2019-01-19 02:07:54 +0800

    0.1

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_destroy();
-echo "<script language='javascript'>
-                    alert('登出成功');
-                    window.open('/semestertool.php', '_self')
-                    </script>";
-
-header("location: index.php");
