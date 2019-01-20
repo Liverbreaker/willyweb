9a4c6acd1932fac487c9ea4abc036df8f29491ed
Liverbreaker <willy083092@gmial.com>
2019-01-20 08:03:26 +0800

    Update loginstatus.php

--- conf/loginstatus.php
+++ conf/loginstatus.php
@@ -4,7 +4,7 @@
 if ( session_status() == PHP_SESSION_NONE ){
 	session_start();
 }
-require_once( 'config.php' );
+require_once( '/conf/config.php' );
 echo "username: ".$_SESSION['username'];
 echo "<br>";
 echo "nickname: ".$_SESSION['nickname'];
